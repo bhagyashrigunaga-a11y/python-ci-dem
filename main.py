3b5d7a4dbfbd914714cda9d6ca8e90b4141e7783
bhagyashrigunaga-a11y <bhagyashrigunaga@gmail.com>
2026-03-17 19:25:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ return a + b
 
 if name == "main":
 print(add(3, 4))
+
+test_main.py
+import unittest
+from main import add
+
+class TestAdd(unittest.TestCase):
+def test_add(self):
+self.assertEqual(add(2, 3), 5)
